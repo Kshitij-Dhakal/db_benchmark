63ee3eb44a36d6ae3df255718721c259dfa850ac
Kshitij Dhakal <dhakalkshitij@gmail.com>
2022-12-10 23:30:05 +0545

    pagination benchmark by offset, row_number and where added

--- mysql/build.gradle.kts
+++ mysql/build.gradle.kts
@@ -17,7 +17,6 @@ repositories {
 
 dependencies {
     implementation(project(":shared"))
-    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.mysql:mysql-connector-j")
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -24,7 +24,7 @@ repositories {
 
 dependencies {
     api("org.springframework.boot:spring-boot-starter-data-jdbc")
-    implementation("org.springframework.boot:spring-boot-starter")
+    api("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
