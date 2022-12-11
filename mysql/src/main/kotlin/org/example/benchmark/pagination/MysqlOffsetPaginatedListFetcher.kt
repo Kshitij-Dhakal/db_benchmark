8ab7e3a976a3d7947bc5e1be9431b88b746f61d9
Kshitij Dhakal <dhakalkshitij@gmail.com>
2022-12-11 19:22:22 +0545

    offset pagination test fix

--- mysql/src/main/kotlin/org/example/benchmark/pagination/MysqlOffsetPaginatedListFetcher.kt
+++ mysql/src/main/kotlin/org/example/benchmark/pagination/MysqlOffsetPaginatedListFetcher.kt
@@ -1,14 +1,18 @@
 package org.example.pagination
 
 import org.example.benchmark.pagination.PaginatedListFetcher
+import org.example.commons.TestConfiguration
 import org.example.commons.entity.Person
 import org.example.commons.repo.PersonRepo
 import org.springframework.stereotype.Repository
 
 @Repository
-class MysqlOffsetPaginatedListFetcher(private val personRepo: PersonRepo) : PaginatedListFetcher<Person> {
+class MysqlOffsetPaginatedListFetcher(
+    private val personRepo: PersonRepo,
+    private val testConfiguration: TestConfiguration
+) : PaginatedListFetcher<Person> {
 
     override fun fetchNext(token: Long): List<Person> {
-        return personRepo.getPersonsLimitAndOffset("createdDate", "ASC", 100, token)
+        return personRepo.getPersonsLimitAndOffset("createdDate", "ASC", testConfiguration.stepSize.toLong(), token)
     }
 }
