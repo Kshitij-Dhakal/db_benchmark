2156c4fd427ea9cfec4067f25f655d6048e2e671
Kshitij Dhakal <dhakalkshitij@gmail.com>
2022-12-11 16:52:38 +0545

    step size bug fixes

--- mysql/src/main/kotlin/org/example/benchmark/pagination/MysqlWherePaginatedListFetcher.kt
+++ mysql/src/main/kotlin/org/example/benchmark/pagination/MysqlWherePaginatedListFetcher.kt
@@ -1,12 +1,21 @@
 package org.example.benchmark.pagination
 
+import org.example.commons.TestConfiguration
 import org.example.commons.entity.Person
 import org.example.commons.repo.PersonRepo
 import org.springframework.stereotype.Repository
 
 @Repository
-class MysqlWherePaginatedListFetcher(private val personRepo: PersonRepo) : PaginatedListFetcher<Person> {
+class MysqlWherePaginatedListFetcher(
+    private val personRepo: PersonRepo,
+    private val testConfiguration: TestConfiguration
+) : PaginatedListFetcher<Person> {
     override fun fetchNext(token: Long): List<Person> {
-        return personRepo.getPersonsWhereBetween("createdDate", "ASC", token, token + 99)
+        return personRepo.getPersonsWhereBetween(
+            "createdDate",
+            "ASC",
+            token,
+            token + testConfiguration.stepSize.toLong() - 1
+        )
     }
 }
