487d35449a682c8aa1f4ed49592b623d8bff178b
Kshitij Dhakal <dhakalkshitij@gmail.com>
2022-12-11 16:51:53 +0545

    add in batch for faster performance

--- mysql/src/main/kotlin/org/example/benchmark/pagination/MysqlRowNumberPaginatedListFetcher.kt
+++ mysql/src/main/kotlin/org/example/benchmark/pagination/MysqlRowNumberPaginatedListFetcher.kt
@@ -1,12 +1,21 @@
 package org.example.benchmark.pagination
 
+import org.example.commons.TestConfiguration
 import org.example.commons.entity.Person
 import org.example.commons.repo.PersonRepo
 import org.springframework.stereotype.Repository
 
 @Repository
-class MysqlRowNumberPaginatedListFetcher(private val personRepo: PersonRepo) : PaginatedListFetcher<Person> {
+class MysqlRowNumberPaginatedListFetcher(
+    private val personRepo: PersonRepo,
+    private val testConfiguration: TestConfiguration
+) : PaginatedListFetcher<Person> {
     override fun fetchNext(token: Long): List<Person> {
-        return personRepo.getPersonsRowNumberBetween("createdDate", "ASC", token, token + 999)
+        return personRepo.getPersonsRowNumberBetween(
+            "createdDate",
+            "ASC",
+            token,
+            token + testConfiguration.stepSize.toLong() - 1
+        )
     }
 }
