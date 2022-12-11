4b649d5d15230a2dd14dd3de178ce4f32c457c91
Kshitij Dhakal <dhakalkshitij@gmail.com>
2022-12-11 19:59:07 +0545

    offset pagination test fix

--- mysql/src/main/kotlin/org/example/MysqlTestConfiguration.kt
+++ mysql/src/main/kotlin/org/example/MysqlTestConfiguration.kt
@@ -3,15 +3,23 @@ package org.example
 import jakarta.annotation.PostConstruct
 import org.example.commons.TestConfiguration
 import org.example.commons.adder.PersonDataAdder
-import org.springframework.stereotype.Service
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
-@Service
+@Configuration
 class MysqlTestConfiguration(
     private val personDataAdder: PersonDataAdder,
-) : TestConfiguration(sampleSize = 1_00_000, stepSize = 1000) {
+) : TestConfiguration(sampleSize = 1_000_000, stepSize = 10000) {
 
     @PostConstruct
     fun addPersonData() {
         personDataAdder.addNPersons(sampleSize)
+    }
+
+    @Bean
+    fun getExecutorService(): ExecutorService {
+        return Executors.newFixedThreadPool(10)
     }
 }
